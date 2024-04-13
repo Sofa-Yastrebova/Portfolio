ed0c877dacc964647468dd71f6f031574a89b6c5
Sofia Yastrebova <s89055747477@icloud.com>
2024-04-13 11:53:10 +0300

    feat: add animstion for skills section

--- js/animation.js
+++ js/animation.js
@@ -8,3 +8,17 @@ tlStart.from(".author",{
     scale: 4,
     ease:"bounce(1.5)",
 });
+
+gsap.from('.skill-card',{
+    stagger:0.3,
+    opacity:0,
+    y:200,
+    scrollTrigger:{
+        // markers:true,
+        trigger: '.skills',
+        start: 'top 60%',
+        end:'30% 50%',
+        scrub: 2,
+        toggleActions: 'play none none reverse',
+    }
+})
